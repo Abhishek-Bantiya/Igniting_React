ccb2a881ee0080c14f6624099e3ddf1538336b8d
Abhishek-Bantiya <abhishekbantiya.1rn19ec003@gmail.com>
2023-12-19 23:06:54 +0530

    update jsx

--- App.js
+++ App.js
@@ -18,19 +18,28 @@ import ReactDOM from 'react-dom/client'
 // zero config 
  
 
-const heading = React.createElement(
-    "h1",
-    {
-        id: "header"
-    },
-    "Hi, this is the Heading"
-);
-const main = React.createElement(
-    "main", {
-        id: "main"
-    }, "Hi, this is the main section"
-);
+// const heading = React.createElement(
+//     "h1",
+//     {
+//         id: "header"
+//     },
+//     "Hi, this is the Heading"
+// );
+// const main = React.createElement(
+//     "main", {
+//         id: "main"
+//     }, "Hi, this is the main section"
+// );
+// const heading2 = (
+//     <h1 id="heading2" key="key2">Namaste react 2</h1>
+// );
+// console.error(heading)
+// console.error(heading2)
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render([heading, main]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render([heading, main]);
+//JSX syntax
+const jsx = <h1>React using js</h1>
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
+root.render(jsx)
